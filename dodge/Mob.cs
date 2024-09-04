3c97d41b17da5b81738936a24794ec97adc73fdb
shiganaiman <shiganaiman@gmail.com>
2024-09-04 15:16:48 +0900

    fix cs

--- dodge/Mob.cs
+++ dodge/Mob.cs
@@ -9,10 +9,13 @@ public partial class Mob : RigidBody2D
     var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
     string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
     animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+
+    GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D").ScreenExited += OnVisibleOnScreenNotifier2DScreenExited;
   }
 
   private void OnVisibleOnScreenNotifier2DScreenExited()
   {
+    GD.Print("OnVisibleOnScreenNotifier2DScreenExited");
     QueueFree();
   }
 }
